d725f08c94ee0f4262ae0e05e0cfee18c7aa3df0
Jyotsna Prakash <jsprakas@ucsc.edu>
2015-11-04 17:50:20 -0800

    initial creation of library app
    Displays the index page from sprint 1.

--- soundstash/urls.py
+++ soundstash/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from index import views
+import library.views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', library.views.index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
 ]
