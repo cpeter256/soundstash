341746162c3209c230a8c30c1fac0bebee4b517d
CPeter <cpeter256@gmail.com>
2015-11-07 22:24:06 -0800

    Sorting is now functional, also replaced test json with uglier but more voluminous data

--- library_json/views.py
+++ library_json/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import json
 
-json_data = '[{"title":"foo","url":"blah.bar"},{"title":"baz","url":"quux.quuux"}]'
+json_data = '[{"title":"0ozdignzo5bp6do1b22b792a","url":"whzzmw3fvwvobu"},{"title":"3_mtu5kq0cx0","url":"t8iaqwbxse6b7"},{"title":"3vdpqp3x1k","url":"6ryo2y93d00"},{"title":"9ci7pqt8mon__5jp","url":"hnngkoe3ob1g.h"},{"title":"sn4jyrkgzwg3v","url":"o7cjf7o0cva9i6zwhrzqsrpm"},{"title":"zosje0cfxxtd","url":"4vowbp4z_oi36s_cdts"},{"title":".mraljsmrxobs.","url":"elp.v.rv48"},{"title":"tr_.1lla31h","url":"xrtlhjlkv0_0cg3oour"},{"title":"qcxjr.s3","url":"2wk5icvs_le077hp"},{"title":"d_r3bd","url":"rwoi.qbilgg41"},{"title":"2bltp0j.jcf","url":"ch9ah"},{"title":"kbbw984","url":"jieyyi"},{"title":"sgpr4n.yru1wlk05c","url":"t15x2z2s0"},{"title":"ze17osv943g","url":"u98ry102y36qkwrjfla5qo"},{"title":"7zlvnj34vh3","url":"cp0elg3mar"},{"title":"l73f378w3hqonev8","url":"y6obbf.jwz4"},{"title":"x_0rflwxmfl.s","url":"nb4qkgrrtn49w82dddfjkx"},{"title":"uz2n79xil6j30.1m0xf","url":"helap7elksr9ybn9fl"},{"title":"55wt9udh9qaczq","url":"gixscc.7qkf1kgtn_omp0y"},{"title":"l2i7gbfa53u","url":"mxowf"}]'
 
 data = json.loads(json_data)
 
--- static/library.js
+++ static/library.js
@@ -1,5 +1,107 @@
 "use strict";
 
+var SORT_TITLE_ASC = 1;
+var SORT_TITLE_DEC = 2;
+var SORT_ARTIST_ASC = 3;
+var SORT_ARTIST_DEC = 4;
+
+var sortmode = 0;
+
+function bullshit() {
+	var len = Math.floor(Math.random()*20)+5;
+	var shits = "qwertyuiopasdfghjklzxcvbnm1234567890._";
+	var s = "";
+	for (var i = 0; i < len; i++) {
+		var l = shits.charAt(Math.floor(Math.random()*shits.length));
+		s += l;
+	}
+	return s;
+}
+
+function make_testjson() {
+	var test = [];
+	for (var i = 0; i < 20; i++) {
+		var tests = {};
+		tests.title = bullshit();
+		tests.url = bullshit();
+		test.push(tests);
+	}
+	console.log(JSON.stringify(test));
+}
+
+function toggleSortTitle() {return toggleSort(true);}
+function toggleSortArtist() {return toggleSort(false);}
+
+function toggleSort(title) {
+	var tca = document.getElementById("titlesort-chev-up"); tca.style.display = "none";
+	var tcd = document.getElementById("titlesort-chev-down"); tcd.style.display = "none";
+	var aca = document.getElementById("artistsort-chev-up"); aca.style.display = "none";
+	var acd = document.getElementById("artistsort-chev-down"); acd.style.display = "none";
+	
+	if (title) {
+		if (sortmode == SORT_TITLE_ASC) {
+			sortmode = SORT_TITLE_DEC;
+			tcd.style.display = "";
+		} else {
+			sortmode = SORT_TITLE_ASC;
+			tca.style.display = "";
+		}
+	} else {
+		if (sortmode == SORT_ARTIST_ASC) {
+			sortmode = SORT_ARTIST_DEC;
+			acd.style.display = "";
+		} else {
+			sortmode = SORT_ARTIST_ASC;
+			aca.style.display = "";
+		}
+	}
+	
+	updateSort();
+}
+
+function updateSort() {
+	var tbody = document.getElementById("lib-table").getElementsByTagName("tbody")[0];
+	var col, asc;
+	switch (sortmode) {
+		case SORT_TITLE_ASC:
+			col = 0;
+			asc = 1;
+			break;
+		case SORT_TITLE_DEC:
+			col = 0;
+			asc = -1;
+			break;
+		case SORT_ARTIST_ASC:
+			col = 1;
+			asc = 1;
+			break;
+		case SORT_ARTIST_DEC:
+			col = 1;
+			asc = -1;
+			break;
+		default: return;
+	}
+	
+	var rows = tbody.rows, rlen = rows.length, arr = new Array(), i, j, cells, clen;
+	// fill the array with values from the table
+	for(i = 0; i < rlen; i++){
+	cells = rows[i].cells;
+	clen = cells.length;
+	arr[i] = new Array();
+		for(j = 0; j < clen; j++){
+			arr[i][j] = cells[j].innerHTML;
+		}
+	}
+	// sort the array by the specified column number (col) and order (asc)
+	arr.sort(function(a, b){
+		return (a[col] == b[col]) ? 0 : ((a[col] > b[col]) ? asc : -1*asc);
+	});
+	for(i = 0; i < rlen; i++){
+		arr[i] = "<td>"+arr[i].join("</td><td>")+"</td>";
+	}
+	tbody.innerHTML = "<tr>"+arr.join("</tr><tr>")+"</tr>";
+}
+
 function loadSongs() {
 	var songreq = new XMLHttpRequest();
 	songreq.onreadystatechange = function(e) {
@@ -21,7 +123,7 @@ function addSongs(songs) {
 	var tbody = table.getElementsByTagName("tbody")[0];
 	for (var si in songs) {
 		var song = songs[si];
-		console.log(tbody);
+		//console.log(tbody);
 		var row = document.createElement("tr");
 			var titledata = document.createElement("td");
 				titledata.innerHTML = song.title;
