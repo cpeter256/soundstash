d14e34b355782c617bca007dd21ec5e4d2134753
Jyotsna Prakash <jsprakas@ucsc.edu>
2015-11-12 13:36:16 -0800

    create User table
    as part of library app.

--- library/models.py
+++ library/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+# not sure about my PK choices
+
 # Create your models here.
 class Sound(models.Model):
     url = models.URLField(unique=True, blank=False, primary_key=True) # TODO index? PK?
@@ -8,3 +10,10 @@ class Sound(models.Model):
 
     def __str__(self):
         return "%s (%s)" % (self.uri, self.title)
+
+class User(models.Model):
+    userid = models.CharField(max_length=200, unique=True, primary_key=True) # pk??
+    sounds = models.ManyToManyField(Sound)
+    def __str__(self):
+        return str(self.userid)
+
