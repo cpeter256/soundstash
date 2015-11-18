6fb2d691a8e2ae2b61f927cef96a30b0d5add648
Justin Ewing <jpewing@ucsc.edu>
2015-11-17 17:16:34 -0800

    Reformatted json_data->soundstash, fixed library_json view.py

--- library_json/views.py
+++ library_json/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import json
+import os
 
-#filename = 'json_data2'
-#json_file = open(filename, 'r')
-#json_data = json_file.read()
+filename = 'json_data'
+file_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), filename)
+json_file = open(file_path, 'r')
+json_data = json_file.read()
 
-json_data = '[{"title":"__TITLE__","url":"__URL__","artist":"__ARTIST__"},{"title":"__TITLE__","url":"__URL__","artist":"__ARTIST__"}]'
+#json_data = '[{"title":"__TITLE__","url":"__URL__","artist":"__ARTIST__"},{"title":"__TITLE__","url":"__URL__","artist":"__ARTIST__"}]'
 
 data = json.loads(json_data)
 
