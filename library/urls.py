69154930202142df14fe323f69300eea4de635f4
Jyotsna Prakash <jsprakas@ucsc.edu>
2015-11-21 12:32:55 -0800

    [trivial] smarter slug matching
    match "[\w-]+" instead of ".+"

--- library/urls.py
+++ library/urls.py
@@ -6,6 +6,6 @@ import library_json.views
 urlpatterns = [
 #    url(r'$', library_json.views.libraryView)
     url(r'add/$|add.html', add_song),
-    url(r'(?P<playlist_name>.+)/library_json', library_json.views.libraryView),
-    url(r'.+/$', playlist),
+    url(r'^(?P<playlist_name>[\w-]+)/library_json/$', library_json.views.libraryView),
+    url(r'^[\w-]+/$', playlist),
 ]
