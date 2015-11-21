08b2106b35273205ab449a9534606890d015a0e9
Jyotsna Prakash <jsprakas@ucsc.edu>
2015-11-21 12:37:43 -0800

    [trivial] route library/ to library/default

--- library/urls.py
+++ library/urls.py
@@ -4,8 +4,8 @@ from .views import *
 import library_json.views
 
 urlpatterns = [
-#    url(r'$', library_json.views.libraryView)
     url(r'add/$|add.html', add_song),
+    url(r'^library_json/$', library_json.views.libraryView),
     url(r'^(?P<playlist_name>[\w-]+)/library_json/$', library_json.views.libraryView),
-    url(r'^[\w-]+/$', playlist),
+    url(r'^$|^[\w-]+/$', playlist),
 ]
