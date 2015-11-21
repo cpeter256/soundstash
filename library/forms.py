a2b9357a278be32eac2ddd18df7c3c12c680004a
Jyotsna Prakash <jsprakas@ucsc.edu>
2015-11-21 06:49:46 -0800

    [trivial] use URLField in form

--- library/forms.py
+++ library/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class AddSongForm(forms.Form):
-    url = forms.CharField(label='Link', max_length=100) # TODO max len?
+    url = forms.URLField(label='Link', max_length=100) # TODO max len?
     artist = forms.CharField(label='Artist')
     title = forms.CharField(label='Title')
