51c7e55adb04424c26fdbd98b53ca91a62619c31
Jyotsna Prakash <jsprakas@ucsc.edu>
2015-11-21 10:53:03 -0800

    implement Very Basic url routing for playlists
    Route /library/default to display "default" playlist belonging to
    "thelatecomers" user
    
    json data now found at /library/default/library_json

--- library/urls.py
+++ library/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 
 from .views import *
+import library_json.views
 
 urlpatterns = [
     url(r'add/$|add.html', add_song),
+    url(r'default/$', index),
+    url(r'default/library_json', library_json.views.libraryView),
 ]
