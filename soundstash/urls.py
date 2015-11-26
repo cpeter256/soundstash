36fc7e056d84d20607b54372e66332c7c1d699b7
Jyotsna Prakash <jsprakas@ucsc.edu>
2015-11-25 19:51:15 -0800

    Return JSON of playlist names
    Fix #12

--- soundstash/urls.py
+++ soundstash/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     url(r'^library_json/', include('library_json.urls')),
     url(r'^login/$', views.login),
     url(r'^logout/$', views.logout),
-    url(r'^playlists/all/$', library.views.list_of_playlists),
+    url(r'^playlists/all/$', library.views.playlists_json),
     url(r'^$', RedirectView.as_view(url='library/default',permanent=True)),
     url(r'^library/', include('library.urls')),
     url(r'^admin/', include(admin.site.urls)),
