bcbfd1527686d451c328c312a7e35a0c9fec6092
Justin Ewing <jpewing@ucsc.edu>
2015-11-25 21:02:23 -0800

    creates a default playlist

--- registration/views.py
+++ registration/views.py
@@ -4,25 +4,34 @@ from django.shortcuts import render, render_to_response
 from django.http import HttpResponseRedirect
 
 from .forms import userForm
+from library.models import Playlist
 
 def register(request):
 	if request.method == "POST":
-		form = userForm(request.POST)
+		#grab user data //potentially not safe
 		first_name = request.POST.get('First_name')
 		last_name  = request.POST.get('Last_name')
 		usr_name   = request.POST.get('username')
 		email      = request.POST.get('email')
 		password   = request.POST.get('password')
 
+		#create the user object
 		new_user  = User.objects.create_user(usr_name, email, password)
         	new_user.first_name = first_name
 		new_user.last_name  = last_name
 		new_user.save()
 
+		#instantiate a default playlist
+		p = Playlist(name='default', owner=new_user)
+		p.save()
+
 		#this is prob bad code
-		user = auth.authenticate(username=usr_name, password=password)
-		auth.login(request, user)
-		return HttpResponseRedirect('/')
+		try:
+			user = auth.authenticate(username=usr_name, password=password)
+			auth.login(request, user)
+			return HttpResponseRedirect('/')
+		except IntegrityError:
+			raise Http404('User already exists')
 	else:
 		form = userForm()
 		
