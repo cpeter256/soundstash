ac95255c97c6810cf297a378f7997e44f126e78a
Jyotsna Prakash <jsprakas@ucsc.edu>
2015-11-26 00:51:57 -0800

    Display playlist name in UI
    via django templating

--- library/views.py
+++ library/views.py
@@ -14,37 +14,38 @@ def index(request):
 
 @login_required
 def playlist_view(request, playlist='default'):
-    if (request.method == 'POST'):
-        # TODO handle failures
-        artist = request.POST['artist']
-        title = request.POST['title']
-        url = request.POST['url']
-        # TODO want to check if user has this song in another playlist
-        # and re-use it if they do!
-        # BUT don't want to share song objs amongst users...
-        # what if one user edits the song info? it would change for everyone else
-        s = Sound(url=url, title=title, artist=artist)
-        s.save()
-        # TODO handle nonexistant playlist
+    try:
         p = Playlist.objects.get(owner=request.user,
                                  slug=playlist)
-        p.sound.add(s)
-        p.save()
-        return HttpResponse()
-    elif (request.method == 'DELETE'):
-        # TODO check for existence
-        try:
-            p = Playlist.objects.get(owner=request.user,
-                                 slug=playlist)
-            songs = Sound.objects.filter(playlist=p)
-            # TODO both these deletes are necessary right
-            songs.delete()
-            p.delete()
-        except Playlist.DoesNotExist:
-            raise HttpResponse(status=400)
-        return HttpResponse()
-    else:
-        return render(request,'index.html')
+        if (request.method == 'POST'):
+            # TODO handle failures
+            artist = request.POST['artist']
+            title = request.POST['title']
+            url = request.POST['url']
+            # TODO want to check if user has this song in another playlist
+            # and re-use it if they do!
+            # BUT don't want to share song objs amongst users...
+            # what if one user edits the song info? it would change for everyone else
+            s = Sound(url=url, title=title, artist=artist)
+            s.save()
+            # TODO handle nonexistant playlist
+            p.sound.add(s)
+            p.save()
+            return HttpResponse()
+        elif (request.method == 'DELETE'):
+            # TODO check for existence
+            try:
+                songs = Sound.objects.filter(playlist=p)
+                # TODO both these deletes are necessary right
+                songs.delete()
+                p.delete()
+            except Playlist.DoesNotExist:
+                raise HttpResponse(status=400)
+            return HttpResponse()
+        else:
+            return render(request,'index.html', {'current_playlist': p.name})
+    except:
+        raise Http404('Playlist does not exist')
 
 @login_required
 def delete_song(request, playlist_slug, pk):
