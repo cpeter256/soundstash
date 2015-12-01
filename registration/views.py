33d48346a40a1d6b0dd04227f2eddc03a02255eb
Justin Ewing <jpewing@ucsc.edu>
2015-11-30 21:56:33 -0800

    IntegretyError for registered users

--- registration/views.py
+++ registration/views.py
@@ -1,7 +1,8 @@
+from django.db import IntegrityError
 import django.contrib.auth as auth
 from django.contrib.auth.models import User
 from django.shortcuts import render, render_to_response
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 
 from .forms import userForm
 from library.models import Playlist
@@ -14,24 +15,25 @@ def register(request):
 		usr_name   = request.POST.get('username')
 		email      = request.POST.get('email')
 		password   = request.POST.get('password')
-
+		
+		try:
 		#create the user object
-		new_user  = User.objects.create_user(usr_name, email, password)
-		new_user.first_name = first_name
-		new_user.last_name  = last_name
-		new_user.save()
+		    new_user  = User.objects.create_user(usr_name, email, password)
+		    new_user.first_name = first_name
+		    new_user.last_name  = last_name
+		    new_user.save()
 
 		#instantiate a default playlist
-		p = Playlist(name='default', owner=new_user)
-		p.save()
+		    p = Playlist(name='default', owner=new_user)
+		    p.save()
 
 		#this is prob bad code
-		try:
-			user = auth.authenticate(username=usr_name, password=password)
-			auth.login(request, user)
-			return HttpResponseRedirect('/')
+	
+		    user = auth.authenticate(username=usr_name, password=password)
+		    auth.login(request, user)
+		    return HttpResponseRedirect('/')
 		except IntegrityError:
-			raise Http404('User already exists')
+		    raise Http404('User already exists')
 	else:
 		form = userForm()
 		
